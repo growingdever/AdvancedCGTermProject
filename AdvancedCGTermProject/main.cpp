0fc6e8645d3c6d50f30474564e0ae84f4b534544
loki <growingdever@gmail.com>
2014-12-09 00:48:27 +0900

    Initial Commit

---
+++ AdvancedCGTermProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AdvancedCGTermProject
+//
+//  Created by loki on 2014. 12. 9..
+//  Copyright (c) 2014년 loki. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
